bb8af19fbacd46abcd3319e8f13cdfd2df99a33d
M0ck1ngjay178 <bon8330@pennwest.edu>
2025-01-14 15:07:32 -0500

    comments

--- Program1/Average.java
+++ Program1/Average.java
@@ -1,22 +1,25 @@
 package Program1;
 public class Average{
 
-    double grade, totalGrades;
-    int gradeCount;
+    //---------------GLOBAL VARIABLES------------------
+    double grade, totalGrades; // type double variables for grade input and total sumed grades
+    int gradeCount; // type integer counter for # of grades
+    //-------------END GLOBAL VARIABLES------------------
 
-
-
+    
     public Average(){
         //program functioning 
     }
 
 
 
+    /****************MAIN***********************/
     public static void main(String[] args){
 
         System.out.println("!!Welcome to the average program!!");
         new Average();
 
     }
+    /****************END MAIN********************/
     
 }
