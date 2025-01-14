7fa2d9864846a1b28328effcc7aadbe7a1dc1ad3
M0ck1ngjay178 <bon8330@pennwest.edu>
2025-01-14 14:53:15 -0500

    fixed folder issues, initialized documents

---
+++ Program1/Average.java
@@ -0,0 +1,20 @@
+package Program1;
+public class Average{
+
+    double grade;
+
+
+    public Average(){
+        //program functioning 
+    }
+
+
+
+    public static void main(String[] args){
+
+        System.out.println("!!Welcome to the average program!!");
+        new Average();
+
+    }
+    
+}
--- Program1/IntroProgram/JavaTest.java
+++ Program1/IntroProgram/JavaTest.java
@@ -1,4 +1,3 @@
-
 public class JavaTest{
 
     public JavaTest(){
