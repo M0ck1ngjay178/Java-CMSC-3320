70eb3e44747969075141178af0248b82c42923ca
M0ck1ngjay178 <bon8330@pennwest.edu>
2025-01-14 12:05:00 -0500

    cmd practice-lecture1

---
+++ Program1/JavaTest.java
@@ -0,0 +1,13 @@
+
+public class JavaTest{
+
+    public JavaTest(){
+        //empty
+    }
+
+    public static void main(String[] args){
+
+        System.out.println("!!!Hello World!!!");
+
+    }
+}
