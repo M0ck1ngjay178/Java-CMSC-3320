13e9379c6ab90ea8fa28393a7b260bd30f3a9177
M0ck1ngjay178 <bon8330@pennwest.edu>
2025-02-04 14:24:58 -0500

    commmennts ugh

--- Program2/V7/Program2.java
+++ Program2/V7/Program2.java
@@ -42,40 +42,40 @@ class Program2{
 
         //----------OPEN INPUT AND OUTPUT-------------------	        		
 	        infile = new BufferedReader(new FileReader(ionames[0]));	//opens input file
-            outfile = new PrintWriter(new FileWriter(ionames[1]));	//opens output file           
+            outfile = new PrintWriter(new FileWriter(ionames[1]));	    //opens output file           
         //-----------------------------------------------------
 
         //------------WHILE: READ LINE FROM INPUT------------------------
         while ((inbuffer = infile.readLine()) != null){
-            inline = new StringTokenizer(inbuffer, "\t\r\n !@#$%^&*()_/?;:<>.,{}[]\\");
+            inline = new StringTokenizer(inbuffer, "\t\r\n !@#$%^&*()_/?;:<>.,{}[]\\");     //set stringtokenizer with delimeters
             
-            while (inline.hasMoreTokens()) {
-                word = inline.nextToken();
+            while (inline.hasMoreTokens()) {                //while look to run on condition of moreTokens
+                word = inline.nextToken();                  //pull tokens from line read from keayboard
                 
                 // If the token begins with an alpha character
                 if (Character.isLetter(word.charAt(0))) {
-                    check = false;
-                    for (i = 0; i < count; i++) {
-                        if (words[i].isWord(word)) {
-                            words[i].addOne();
-                            check = true;
-                            break;
+                    check = false;                              //set check condition to false
+                    for (i = 0; i < count; i++) {               //for loop to continue until count is greater
+                        if (words[i].isWord(word)) {            //if a word is a work, store it in the words array
+                            words[i].addOne();                  //use of function .addOne()
+                            check = true;                       //set check to true
+                            break;                              //break out of condition
                         }
                     }
-                    if(!check){
-                        words[count] = new Word(word);
-                        count++;
+                    if(!check){                                 //if check is flase
+                        words[count] = new Word(word);          //the word is unique, not found, and needs added to the word array
+                        count++;                                //increment count
                     }
-                } else if(IsPart(word)){    // if word is part int and part string, we need to seperate them
-                    for( i=1; i<word.length(); i++){    // ran this loop just to get the index of the first letter/special character. (COULD GET INDEX FROM ISPART??)
-                        if (Character.isLetter(word.charAt(i))){    //keeps i at the position of the first letter.
+                } else if(IsPart(word)){                                 // if word is part int and part string, we need to seperate them
+                    for( i=1; i<word.length(); i++){                    // ran this loop just to get the index of the first letter/special character. (COULD GET INDEX FROM ISPART??)
+                        if (Character.isLetter(word.charAt(i))){        //keeps i at the position of the first letter.
                             break;
                          }else if(!Character.isLetterOrDigit(word.charAt(i))){
                             break;
                          }
                     }
-                    String word1=null;        // variable to get the number portion
-                        String word2=null;    // variable to get the string portion
+                    String word1=null;                            // variable to get the number portion
+                        String word2=null;                       // variable to get the string portion
                         word1=word.substring(0, i);  //puts the number portion into word1
                         word2=word.substring(i, word.length()); //puts the string portion into word2
                         sum+= Integer.parseInt(word1);        // converts word1 into an int and adds it onto the sum.
@@ -119,11 +119,10 @@ class Program2{
         infile.close();  //close input file
         outfile.close(); //close outputfile
         //----------------------
-        
 
 	}
 	else{
-	    System.err.println("Program Finished and Opened No Files");
+	    System.err.println("Program Finished and Opened No Files"); //print error if no files were opened, triggered by enter
     }
 }
     //-------------ISINT----------------------------------
@@ -140,8 +139,8 @@ class Program2{
     //-------------END ISINT-------------------------------
    
     // ISPART----------------------------
-    static Boolean IsPart(String word){       //function purpose: method to check if string is part int and part string
-        for(int i=1; i<word.length(); i++){    // this function is used after isLetter so know index 0 is not a letter, we can start at index 1, checking each char to see if it's a letter
+    static Boolean IsPart(String word){             //function purpose: method to check if string is part int and part string
+        for(int i=1; i<word.length(); i++){          //this function is used after isLetter so know index 0 is not a letter, we can start at index 1, checking each char to see if it's a letter
             if (Character.isLetter(word.charAt(i))){ //if it is a letter we can return true, the string is part int and part string
                 return true;
              }
@@ -165,55 +164,53 @@ class IOfile{
         BufferedReader keysIn = new BufferedReader(new InputStreamReader(System.in));// open keyboard to read
 		
         try{
-            switch(args.length){    //should this be ioname.length? M.B.B-> NO! i dont think so, this check the string argument list to pull the file names
-                case 0:				//Case if no arguments are in command line
-                    System.out.print("ENTER Input Filename: ");
-                    ioname[0] = keysIn.readLine();
+            switch(args.length){                                    //this check the string argument list to pull the file names
+                case 0:				                                //Case if no arguments are in command line
+                    System.out.print("ENTER Input Filename: ");   //enter input file prompt
+                    ioname[0] = keysIn.readLine();                  //read from sting arguments from cmd
                     
-                    if(ioname[0].equals("")){	                  
+                    if(ioname[0].equals("")){	           //check if ioname has an argument
 	                   return false;  
                     }
                     
-                    while(FileExist(ioname[0])==false) {
-	               	 System.out.println("ERROR: Input File Does Not Exist. Please Enter a Valid Input File");
-	               	  ioname[0] = keysIn.readLine();
+                    while(FileExist(ioname[0])==false) {            //if file does not exits, initiate process of reprompting
+	               	  System.out.println("ERROR: Input File Does Not Exist. Please Enter a Valid Input File");
+	               	  ioname[0] = keysIn.readLine();                //read again, this time read from the keayboard
                 	 }
                 	 
-                    System.out.print("ENTER Output Filename: ");
+                    System.out.print("ENTER Output Filename: ");      //read filename for outputfile
+                    ioname[1] = keysIn.readLine();                      //read from keyboard
                     
-                    ioname[1] = keysIn.readLine(); 
-                    
-		              if(FileExist(ioname[1])== false){
-			           	System.out.println("Input and Output Files Recived");	
-	               		break;
+		              if(FileExist(ioname[1])== false){                          //check if output file exists, by calling fileexists function
+			           	System.out.println("Input and Output Files Recived");	 //if exists, display msg
+	               		break;                                                   //break condition
                   	}
                 	else{
 	                	
-	                	if(FileExist(ioname[1]) == true){ //if output does already exist
-	                		System.out.println("ERROR: Output file already exists Choose one of the following. "); 
+	                	if(FileExist(ioname[1]) == true){                   //if output does already exist
+	                		System.out.println("ERROR: Output file already exists Choose one of the following. "); //display error msg, file already exists
 	                		 	                		
 	                		if(!OptionPick(ioname, keysIn)){			// gives options incase output file already exists
-		                		return false;						//returns false to getnames method to end program 
+		                		return false;						    //returns false to getnames method to end program 
 	                		}                			                		
                     	}						
 		            
-		             System.out.println("Input and Output Files Recived");		           
+		             System.out.println("Input and Output Files Recived");	    //display msg that files now recived        
 	                }
                 
-                    break;
+                    break;                                                         //break out of case0
                 //------------------CASE1------------------------------------------------------
                 case 1:				//Case if only input is in command line 
                 ioname[0] = args[0];
                 	while(FileExist(ioname[0])==false) {
-	               	 System.out.println("ERROR: Input File Does Not Exist. Please Enter a Valid Input File");
-	               	  ioname[0] = keysIn.readLine();
+	               	 System.out.println("ERROR: Input File Does Not Exist. Please Enter a Valid Input File");  //display error msg for input
+	               	  ioname[0] = keysIn.readLine();                         //read valid input file from keyboard
 	               	  
-	               	   if(ioname[0].equals("")){			            
-	                    return false;                    
+	               	   if(ioname[0].equals("")){			        //if user enters equal, break program            
+	                    return false;                                       
                   	  }	               	  
-                	 }
-                                           
-                        System.out.println("Input File Now Successful!");
+                	}
+                        System.out.println("Input File Now Successful!");   //display that input file now sucessfull
                	
 		            System.out.print("ENTER Output Filename: ");  //if length is one, we have the input file name, need output file
                    
@@ -233,7 +230,7 @@ class IOfile{
 	                		System.out.println("ERROR: Output file already exists Choose one of the following. "); 
 	                		 	                		
 	                		if(!OptionPick(ioname, keysIn)){			// gives options incase output file already exists
-		                		return false;						//returns false to getnames method to end program 
+		                		return false;						    //returns false to getnames method to end program 
 	                		}                			                		
                     	}						
 		            
@@ -245,19 +242,19 @@ class IOfile{
                 //INPUT FILE MUST EXIST OUTPUT MUST NOT EXIST
                 
                 //------------------CASE2------------------------------------------------------
-                case 2:							//Case if both arguments are in command line
-               	 if(FileExist(args[0])== true) {
-	               	 ioname[0] = args[0];
+                case 2:							        //Case if both arguments are in command line
+               	 if(FileExist(args[0])== true) {        //check if file exists
+	               	 ioname[0] = args[0];               //if true, pull name from arguments
                 	}
                 	else{ 
 	                		                	
-	                    System.out.println("ERROR!!! Input file doesnt exist, Reprompt: ");//reprompt?
-                        ioname[0] = keysIn.readLine();
-                         if(ioname[0].equals("")){			            
-	                    return false;               
+	                    System.out.println("ERROR!!! Input file doesnt exist, Reprompt: ");   //reprompt
+                        ioname[0] = keysIn.readLine();                                          //read input file from keyboard
+                         if(ioname[0].equals("")){			                            //if user supplies enter, return false to break
+	                    return false;                   
                     }
                     
-                        System.out.println("Input File Now Successful!");
+                        System.out.println("Input File Now Successful!");                  
                  
 	                }
 	                
@@ -339,10 +336,10 @@ class IOfile{
 
     //-------------FILENAME-------------------------------------
     static String FileName(String name){                       //Function Purpose: extract file name from name and return the string file name
-        int startIndex, endIndex;                       //variables to hold positioning
-        startIndex = name.lastIndexOf("\\");        //find filename between \ and last dot of path, store in startIndex
-        endIndex = name.lastIndexOf(".");           //find filename between \ and last dot of path, store in endIndex
-        return name.substring(startIndex+1, endIndex);  //return the found subtring part of the filename
+        int startIndex, endIndex;                              //variables to hold positioning
+        startIndex = name.lastIndexOf("\\");               //find filename between \ and last dot of path, store in startIndex
+        endIndex = name.lastIndexOf(".");                  //find filename between \ and last dot of path, store in endIndex
+        return name.substring(startIndex+1, endIndex);         //return the found subtring part of the filename
     }   
     //-------------END FILENAME---------------------------------
 
@@ -394,12 +391,12 @@ class IOfile{
             System.out.println("------------------------");
         }
         while(look){
-            getChoice = keysIn.readLine();
-            switch (getChoice.toUpperCase()) {
+            getChoice = keysIn.readLine();                          //read choice from user
+            switch (getChoice.toUpperCase()) {                      //force choice to upper case
                 
-                case "B":							// BACKUP WORKS
-                    FileBackup(ioname[1], ".bak");
-                    System.out.println("Backup Created. Please Choose Another Option");
+                case "B":							                // BACKUP WORKS
+                    FileBackup(ioname[1], ".bak");             //use FileBackup function to back up the outputfile, giving it a new extention of .bak  
+                    System.out.println("Backup Created. Please Choose Another Option"); //display completion msg
                     break;
                     
                     
@@ -408,19 +405,19 @@ class IOfile{
                			 System.out.println(ioname[1] + " Has Been Overwritten");
                 		break; // if overwrite i think it just break and use. SWAG :)
                 		
-                case "N":										// New Overwrite works and it makes a new file and uses that instead of the one entered
-                	 System.out.print("Give A New Output File Name: ");
-                    ioname[1] = keysIn.readLine();
-                    if (ioname[1] == null || ioname[1].isEmpty()){
-                        ioname[1] = null; // Quit
-                        System.out.print("THE END IS NEAR ");
+                case "N":										                  // New Overwrite works and it makes a new file and uses that instead of the one entered
+                	 System.out.print("Give A New Output File Name: ");         //prompt for new name for out file
+                    ioname[1] = keysIn.readLine();                                //read new name from keyboard
+                    if (ioname[1] == null || ioname[1].isEmpty()){                //check for empyt or null
+                        ioname[1] = null; // Quit                                 //null will quit
+                        System.out.print("THE END IS NEAR ");                   //exit msg
              
                     }
-                     look = false;
+                     look = false;                                              //set look to false, to exit case
                     break;
                 
-                case "Q":										//QUIT WORKS
-                    System.out.println("Quiting...");
+                case "Q":	//QUIT WORKS!
+                    System.out.println("Quiting...");   //display quit msg
                     return false;
 
                 default:
