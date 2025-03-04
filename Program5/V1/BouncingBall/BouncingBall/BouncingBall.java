82b32e19727d42933bc70a35270615c00d4f58c6
M0ck1ngjay178 <margobonal11010@gmail.com>
2025-03-04 13:10:54 -0500

    started vars, lesson 13

--- Program5/V1/BouncingBall/BouncingBall/BouncingBall.java
+++ Program5/V1/BouncingBall/BouncingBall/BouncingBall.java
@@ -1,5 +1,23 @@
 package BouncingBall;
 
-public class BouncingBall{
+import java.awt.*;
+import java.awt.event.*;
+
+
+public class BouncingBall extends Frame implements WindowListener, MouseListener, MouseMotionListener{
+
+    final int   WinLeft = 10;//left side of frame
+    final int   WinTop = 10;//top of frame
+    Point FrameSize = new Point(640, 400);
+    Panel sheet = new Panel();
+    List list = new List(13);
+
+    BouncingBall(){
+        
+    }
+
+
+
+
     
 }
